be9a74be9e217b5e0972bc0a10f24788e8da7ed2
Tachibana Kiyomi <me@yuru.me>
2022-07-24 05:31:45 +0900

    chore: pre-commit hook

---
+++ apps/site/.lintstagedrc.js
@@ -0,0 +1,10 @@
+const path = require('path');
+
+// https://nextjs.org/docs/basic-features/eslint#lint-staged
+const buildEslintCommand = (filenames) =>
+  `next lint --fix --file ${filenames.map((f) => path.relative(process.cwd(), f)).join(' --file ')}`;
+
+module.exports = {
+  '*.{js,jsx,ts,tsx}': [buildEslintCommand],
+  '*.{js,ts,jsx,tsx,css,md}': 'prettier --write --config ../../prettier.config.js',
+};
