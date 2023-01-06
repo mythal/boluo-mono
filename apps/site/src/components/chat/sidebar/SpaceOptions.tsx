f1a72890e22a8cae9f84fd4444b774a1dfa91489
Tachibana Kiyomi <me@yuru.me>
2023-01-06 18:51:18 +0900

    feat: sidebar style

--- apps/site/src/components/chat/sidebar/SpaceOptions.tsx
+++ apps/site/src/components/chat/sidebar/SpaceOptions.tsx
@@ -1,6 +1,8 @@
 import type { Space } from 'boluo-api';
-import { Settings } from 'boluo-icons';
+import { ChevronDown, ChevronUp, Settings } from 'boluo-icons';
+import clsx from 'clsx';
 import type { FC } from 'react';
+import { useState } from 'react';
 import { useMemo } from 'react';
 import { FormattedMessage } from 'react-intl';
 import { useChatPaneDispatch } from '../../../state/panes';
@@ -14,21 +16,37 @@ interface Props {
 
 export const SpaceOptions: FC<Props> = ({ space, panes }) => {
   const dispatch = useChatPaneDispatch();
+  const [folded, setFold] = useState(true);
   const spaceSettingsPane: SpaceSettingsPane = { type: 'SPACE_SETTINGS', id: space.id, spaceId: space.id };
   const spaceSettingsActive = useMemo(() => panes.findIndex(pane => pane.type === 'SPACE_SETTINGS') !== -1, [panes]);
   return (
-    <>
-      <div className="text-surface-600 py-3 px-4 text-sm border-b border-surface-200">
-        <FormattedMessage defaultMessage="Options for {spaceName}" values={{ spaceName: space.name }} />
-      </div>
-      <SidebarItem
-        icon={<Settings />}
-        active={spaceSettingsActive}
-        onClick={() => dispatch({ type: 'TOGGLE', pane: spaceSettingsPane })}
-        toggle
+    <div className={folded ? '' : 'border-b'}>
+      <button
+        onClick={() => setFold(fold => !fold)}
+        className="flex items-center justify-between w-full text-surface-600 py-3 px-4 text-sm border-b border-surface-200 group cursor-pointer hover:bg-surface-100"
       >
-        <FormattedMessage defaultMessage="Space Settings" />
-      </SidebarItem>
-    </>
+        <FormattedMessage defaultMessage="Space Options" />
+        <span
+          className={clsx(
+            'p-1 border rounded-md bg-surface-50',
+            folded ? 'group-hover:border-surface-300' : 'border-surface-400',
+          )}
+        >
+          {folded ? <ChevronDown /> : <ChevronUp />}
+        </span>
+      </button>
+      {!folded && (
+        <>
+          <SidebarItem
+            icon={<Settings />}
+            active={spaceSettingsActive}
+            onClick={() => dispatch({ type: 'TOGGLE', pane: spaceSettingsPane })}
+            toggle
+          >
+            <FormattedMessage defaultMessage="Space Settings" />
+          </SidebarItem>
+        </>
+      )}
+    </div>
   );
 };
